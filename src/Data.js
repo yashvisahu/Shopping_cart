6bd7fac459e2b688e2e139199ce6eb231fae56d7
yashvisahu <108457554+yashvisahu@users.noreply.github.com>
2022-08-23 13:47:17 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Levi's-green color medium size casual shirt.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Tommy Hilfiger-two pieces of large size office shirt.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "H&M-casual white T-shirt.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Jack & Jones-men suit.",
+    img: "images/img-4.jpg",
+  },
+];
